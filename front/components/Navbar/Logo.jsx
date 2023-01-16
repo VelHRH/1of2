1d382475bf10df5f8118b6b190f0b98f3883c3f4
Valentine Yurchenko <45362610+VelHRH@users.noreply.github.com>
2023-01-16 15:20:36 +0300

    open/close menu

--- front/components/Navbar/Logo.jsx
+++ front/components/Navbar/Logo.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
-export const Logo = () => {
+export const Logo = (props) => {
+ const { openMenu, isOpened } = props;
  return (
-  <div className="text-3xl pt-3 pb-7 cursor-pointer font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-sky-500 to-blue-600">
-   1/2
+  <div
+   onClick={openMenu}
+   className={`text-3xl pt-3 pb-7 cursor-pointer font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-sky-500 to-blue-600`}
+  >
+   {isOpened ? "1 of 2" : "1/2"}
   </div>
  );
 };
--- front/components/Navbar/Navbar.jsx
+++ front/components/Navbar/Navbar.jsx
@@ -1,13 +1,57 @@
+import { useState } from "react";
 import { Logo } from "./Logo";
 
 export const Navbar = () => {
+ const [isOpened, setIsOpened] = useState(false);
+ const [active, setActive] = useState("categories");
+
+ const openMenu = () => {
+  setIsOpened(!isOpened);
+ };
  return (
   <div className="flex flex-col items-center px-3 bg-slate-100 text-slate-400">
-   <Logo />
-   <div className="pt-7 border-t-2 border-slate-400 flex flex-col px-3 text-2xl cursor-pointer items-center">
-    <i className="fa-solid fa-house pb-7"></i>
-    <i class="fa-solid fa-briefcase pb-7"></i>
-    <i class="fa-solid fa-user-group pb-7"></i>
+   <Logo openMenu={openMenu} isOpened={isOpened} />
+   <div
+    className={`pt-7 border-t-2 border-slate-400 flex flex-col px-3 text-2xl cursor-pointer ${
+     isOpened ? "items-start" : "items-center"
+    } `}
+   >
+    <div
+     onClick={() => {
+      setActive("categories");
+     }}
+     className={`flex items-center mb-7 ${
+      active === "categories" &&
+      "text-transparent bg-clip-text bg-gradient-to-r from-sky-500 to-blue-600"
+     }`}
+    >
+     <i className={`fa-solid fa-house ${isOpened && "mr-2"}`}></i>
+     <div className="text-xl">{isOpened && "categories"}</div>
+    </div>
+    <div
+     onClick={() => {
+      setActive("creations");
+     }}
+     className={`flex items-center mb-7 ${
+      active === "creations" &&
+      "text-transparent bg-clip-text bg-gradient-to-r from-sky-500 to-blue-600"
+     }`}
+    >
+     <i class={`fa-solid fa-briefcase ${isOpened && "mr-3"}`}></i>
+     <div className="text-xl">{isOpened && "creations"}</div>
+    </div>
+    <div
+     onClick={() => {
+      setActive("users");
+     }}
+     className={`flex items-center mb-7 ${
+      active === "users" &&
+      "text-transparent bg-clip-text bg-gradient-to-r from-sky-500 to-blue-600"
+     }`}
+    >
+     <i class={`fa-solid fa-user-group ${isOpened && "mr-2"}`}></i>
+     <div className="text-xl">{isOpened && "users"}</div>
+    </div>
    </div>
   </div>
  );
