50352aa1b8a3a1c9b4a6eee90798a7514f845fec
Valentine Yurchenko <45362610+VelHRH@users.noreply.github.com>
2023-01-25 13:30:07 +0300

    registration

--- front/pages/registration.js
+++ front/pages/registration.js
@@ -1,54 +1,95 @@
 import {useState} from 'react'
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const regisration = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [imgurl, setImgurl] = useState();
+  const [err, setErr] = useState("");
+  const router = useRouter();
+
+  const submit = async (e) => {
+    e.preventDefault();
+    const res = await fetch(`http://localhost:4444/register`, {
+     method: "POST",
+     headers: {
+      "Content-Type": "application/json",
+     },
+     body: JSON.stringify({
+      email,
+      login: name,
+      password,
+      imgUrl: imgurl || "https://static.vecteezy.com/system/resources/previews/002/318/271/original/user-profile-icon-free-vector.jpg"
+     }),
+    });
+    const data = await res.json();
+  
+    if ("token" in data) {
+     window.localStorage.setItem("token", data.token);
+    } else {
+     setErr(data.message);
+     return;
+    }
+    await router.replace("/login");
+   };
+
   return (
     <div className="absolute top-0 left-0 bg-gradient-to-r from-cyan-500 to-blue-600 w-full h-full overflow-hidden">
    <div className="flex flex-col px-10 w-11/12 md:w-1/3 left-[50%] top-[50%] absolute bg-slate-100 translate-y-[-50%] items-center translate-x-[-50%] text-xl rounded-xl">
     <div className="my-5 text-2xl">Register</div>
-    <form>
+    <form onSubmit={submit}>
      <input
       type="text"
       placeholder="Your email..."
+      onChange={(e) => setEmail(e.target.value)}
+      value={email}
       className={`bg-slate-100 w-full border-b-2 ${
-       Boolean(true) ? "text-red-600" : "text-blue-600"
+        err !== "" && "text-red-600" 
       } ${
-       Boolean(true)
+        err !== ""
         ? "border-red-600"
-        : "border-violet-400"
+        : "border-blue-400"
       } my-5 outline-none focus:border-blue-600`}
      ></input>
      <input
       type="text"
       placeholder="Nickname..."
+      onChange={(e) => setName(e.target.value)}
+      value={name}
       className={`bg-slate-100 w-full border-b-2 ${
-       Boolean(true) ? "text-red-600" : "text-blue-600"
+        err !== "" && "text-red-600" 
       } ${
-       Boolean(true)
+        err !== ""
         ? "border-red-600"
-        : "border-violet-400"
+        : "border-blue-400"
       } my-5 outline-none focus:border-blue-600`}
      ></input>
      <input
       type="password"
       placeholder="Password..."
+       onChange={(e) => setPassword(e.target.value)}
+      value={password}
       className={`bg-slate-100 w-full border-b-2 ${
-       Boolean(true) ? "text-red-600" : "text-blue-600"
+        err !== "" && "text-red-600" 
       } ${
-       Boolean(true)
+        err !== "" 
         ? "border-red-600"
-        : "border-violet-400"
-      } my-5 outline-none focus:border-blue-800`}
+        : "border-blue-400"
+      } my-5 outline-none focus:border-blue-600`}
      ></input>
      <input
       type="text"
-      placeholder="Avatar URL..."
+      placeholder="Avatar URL... (optional)"
+      onChange={(e) => setImgurl(e.target.value)}
+      value={imgurl}
       className={`bg-slate-100 w-full border-b-2 ${
-       Boolean(true) ? "text-red-600" : "text-blue-600"
+        err !== "" && "text-red-600"
       } ${
-       Boolean(true)
+        err !== ""
         ? "border-red-600"
-        : "border-violet-400"
+        : "border-blue-400"
       } my-5 outline-none focus:border-blue-600`}
      ></input>
      <button
